0e917692f18b55ca6d767a90f41c38f58116f29d
bellca225 <nywoo0225@gmail.com>
2024-06-18 06:51:22 +0900

    단가 적용

--- views/page3_view.py
+++ views/page3_view.py
@@ -2,6 +2,58 @@ import tkinter as tk
 from tkinter import ttk
 
 class Page3View(ttk.Frame):
+    # 주호 변경 예정
+    # data['twisted_cable']['4p']
+    # data['connector_jack']['RS-232C(10Pin)']
+    # data['control_cable']['2C']['2.5mm']
+    # data['Communication_premises_power_cable']['25mm']
+    # 마지막 FR 케이블은 현영이한테 확인받을 거임
+    
+    품셈 = {
+        '꼬임 케이블 포설' : 1, 
+        '커넥터 및 Jack 접속' : 2, 
+        '제어 케이블' : 3,
+        '통신용 구내 전력케이블' : 4, 
+        'FR 케이블' : 5
+        }
+    노무임 = {
+        '꼬임 케이블 포설' : '통신케이블공', 
+        '커넥터 및 Jack 접속' : '통신내선공', 
+        '제어 케이블' : '통신케이블공',
+        '통신용 구내 전력케이블' : '통신케이블공', 
+        'FR 케이블' : '통신케이블공'
+        }
+    raw_data = {
+        '간접노무비': 12.2, 
+        '기타경비': 5.8, 
+        '일반관리비': 6.0, 
+        '이윤': 15.0, 
+        '고용보험료': 1.57, 
+        '건강보험료': 3.545, 
+        '노인장기요양보험료': 12.81, 
+        '연금보험료': 4.5, 
+        '산재보험료' : 3.7, 
+        'twisted_cable': {'4p': 0.15}, 
+        'connector_jack': {'RS-232C(10Pin)': 0.49, 
+        'Modular(RJ45-8PinPlug)': 0.13, 
+        'Modular(Outlet)': 0.28, 
+        'TELCO(50Pin)': 1.19, 
+        'Data Line': 0.84}, 
+        'control_cable': {'2C': {'2.5mm': 0.1, }}, #여기 } 안 닫혀서 추가함
+        '통신내선공': 267277, 
+        '통신설비공': 296882, 
+        '통신외선공': 387376, 
+        '통신케이블공': 414944, 
+        '무선안테나공': 339642, 
+        '광케이블설치사': 444142, 
+        'H/W시험사': 375020, 
+        'S/W시험사': 433747, 
+        '통신관련기사': 305806, 
+        '통신관련산업기사': 294019, 
+        '통신관련기능사': 242587}
+    
+    var_selected_texts = []
+    
     ynConstructDate = ''
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
@@ -13,7 +65,6 @@ class Page3View(ttk.Frame):
         relx = 0.05
         lbs = ['작업', '수량', '단가', '금액']
         for lb in lbs :
-            print(lb)
             lb = ttk.Label(self, text = lb)
             lb.place(relx = relx, rely = 0.1)
             relx += 0.25
@@ -48,6 +99,7 @@ class Page3View(ttk.Frame):
         self.labels.append(title)
         total_cost = 0
         for idx in range(len(combined_labels)):
+            
             label_text = combined_labels[idx]
             label = ttk.Label(self, text=label_text)
             label.place(relx = 0.05, rely = 0.57 + idx * 0.03)
@@ -69,12 +121,19 @@ class Page3View(ttk.Frame):
     
     def on_cnt_confirm(self):
         print(1)
+        
 
     def on_next(self):
         self.controller.show_frame("Page4View")
 
-    def update_data(self, selected_texts, decision):
-        self.ynConstructDate = decision
+    def update_data(self, selected_texts, decision = None):
+        self.var_selected_texts = selected_texts
+        품셈_list = [self.품셈[key] for key in selected_texts]
+        노무임1_list = [self.노무임[key] for key in selected_texts]
+        노무임2_list = [self.raw_data[key] for key in 노무임1_list]
+        
+        if decision != None :
+            self.ynConstructDate = decision
         
         # 기존에 생성된 위젯이 있다면 모두 제거
         for label in self.labels:
@@ -100,8 +159,7 @@ class Page3View(ttk.Frame):
             self.entries.append(entry)
             entry.insert(0, "1")
             
-            # 노무임.hwp * 품샘.pdf 넣어줘야 함!
-            단가 = 100
+            단가 = 품셈_list[idx] * 노무임2_list[idx]
             label2 = ttk.Label(self, text = 단가)
             label2.place(relx=0.55, rely=0.15 + idx * 0.035)
             self.labels.append(label2)
