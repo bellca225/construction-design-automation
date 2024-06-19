5ddc99375c1c5acc7d0b297da02a835ac718127a
bellca225 <nywoo0225@gmail.com>
2024-06-19 23:06:28 +0900

    설명 추가

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # construction-design-automation
 
-공사 설계 자동화 프로그램
-main.py 에서 실행하면서 시작
+공사 설계 자동화 프로그램입니다.
+
+터미널에 아래 순서대로 입력하시오.
+
+1. pip freeze > requirements.txt
+2. main.py
+
+현재 데이터 용량 문제로 인해 약 1분 30초 후 프로그램이 실행됩니다.
--- dataexporter.py
+++ dataexporter.py
@@ -1,11 +1,11 @@
-from filedownloader import downloadFiles;
-from excelparser import getData as excelData;
-from pdfparser import getData as pdfData;
-from hwpparser import getData as hwpData;
+from filedownloader import downloadFiles
+from excelparser import getData as excelData
+from pdfparser import getData as pdfData
+from hwpparser import getData as hwpData
 
 # juho : 파일다운로드, 엑셀, pdf, hwp 파일 파싱을 한번에 진행시키는 함수
 def getData():
-    downloadFiles();
+    downloadFiles()
     data = {}
     data.update(excelData())
     data.update(pdfData())
